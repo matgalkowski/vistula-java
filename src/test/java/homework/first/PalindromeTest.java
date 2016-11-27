9d99498697ec069a280d70f4c7d1e3f84ecef2ef
Mateusz Gałkowski <mat.galkowski@gmail.com>
2016-11-27 14:28:16 +0100

    Palindrome

---
+++ src/test/java/homework/first/PalindromeTest.java
@@ -0,0 +1,34 @@
+package homework.first;
+
+/**
+ * Created by matgalkowski on 27.11.2016.
+ */
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PalindromeTest {
+    @Test
+    public void shouldCorrectlyRecognizePalindrome() {
+        Assert.assertTrue(Palindrome.checkIfPalindrome("kajak"));
+    }
+
+    @Test
+    public void shouldCorrectlyRecognizeNonPalindrome() {
+        Assert.assertFalse(Palindrome.checkIfPalindrome("someword"));
+    }
+
+    @Test
+    public void shouldBeCaseInsensitiveWhenRecognizingPalindrome() {
+        Assert.assertTrue(Palindrome.checkIfPalindrome("KAJak"));
+    }
+
+    @Test
+    public void emptyStringIsNotPalindrome() {
+        Assert.assertFalse(Palindrome.checkIfPalindrome(""));
+    }
+
+    @Test
+    public void stringContainingOnlyWhiteCharactersIsNotPalindrome() {
+        Assert.assertFalse(Palindrome.checkIfPalindrome("            "));
+    }
+}
